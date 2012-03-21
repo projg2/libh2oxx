559bb459a622b16b0093454d5688e1ddb847293e
Michał Górny <mgorny@gentoo.org>
2012-03-21 07:10:44 +0100

    Implement (v,u,h,s)=f(p,T).

--- src/h2o.cxx
+++ src/h2o.cxx
@@ -11,27 +11,112 @@
 
 #include "h2o"
 
+namespace h2o
+{
+	namespace internals
+	{
+#		include <h2o/region1.h>
+#		include <h2o/region2.h>
+	};
+};
+
 using namespace h2o;
+using namespace h2o::internals;
+
+typedef double (*twoarg_func_t)(double, double);
+
+static double not_supported(double, double)
+{
+	throw std::runtime_error("Requested function not implemented.");
+};
 
 H2O::H2O(double p, double T)
-	: _p(p), _T(T), _region(internals::H2O_REGION_OUT_OF_RANGE)
+	: _p(p), _T(T), _region(H2O_REGION_OUT_OF_RANGE)
 {
 }
 
 void H2O::_determine_region()
 {
-	_region = internals::h2o_region_pT(_p, _T);
+	_region = h2o_region_pT(_p, _T);
 
-	if (_region == internals::H2O_REGION_OUT_OF_RANGE)
+	if (_region == H2O_REGION_OUT_OF_RANGE)
 		throw std::range_error("(p,T) parameters out of range");
 }
 
-double H2O::p() const
+double H2O::p()
 {
 	return _p;
 }
 
-double H2O::T() const
+double H2O::T()
 {
 	return _T;
+}
+
+static twoarg_func_t funcs_v_pT[H2O_REGION_MAX] = {
+	not_supported,
+	h2o_region1_v_pT,
+	h2o_region2_v_pT,
+	not_supported,
+	not_supported,
+	not_supported
+};
+
+double H2O::v()
+{
+	if (!_region)
+		_determine_region();
+
+	return funcs_v_pT[_region](_p, _T);
+}
+
+static twoarg_func_t funcs_u_pT[H2O_REGION_MAX] = {
+	not_supported,
+	h2o_region1_u_pT,
+	h2o_region2_u_pT,
+	not_supported,
+	not_supported,
+	not_supported
+};
+
+double H2O::u()
+{
+	if (!_region)
+		_determine_region();
+
+	return funcs_u_pT[_region](_p, _T);
+}
+
+static twoarg_func_t funcs_h_pT[H2O_REGION_MAX] = {
+	not_supported,
+	h2o_region1_h_pT,
+	h2o_region2_h_pT,
+	not_supported,
+	not_supported,
+	not_supported
+};
+
+double H2O::h()
+{
+	if (!_region)
+		_determine_region();
+
+	return funcs_h_pT[_region](_p, _T);
+}
+
+static twoarg_func_t funcs_s_pT[H2O_REGION_MAX] = {
+	not_supported,
+	h2o_region1_s_pT,
+	h2o_region2_s_pT,
+	not_supported,
+	not_supported,
+	not_supported
+};
+
+double H2O::s()
+{
+	if (!_region)
+		_determine_region();
+
+	return funcs_s_pT[_region](_p, _T);
 }
