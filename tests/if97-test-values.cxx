82f5eb79fa747cb6ad59b67316525bb3596baa32
Michał Górny <mgorny@gentoo.org>
2012-03-24 11:27:12 +0100

    Tests: pass properties to check_vuhs().

--- tests/if97-test-values.cxx
+++ tests/if97-test-values.cxx
@@ -70,17 +70,16 @@ void check_vuhs(const h2o::H2O& obj,
 		double u_expected, double u_precision,
 		double h_expected, double h_precision,
 		double s_expected, double s_precision,
-		const char* arg1, double arg1_val,
-		const char* arg2, double arg2_val)
+		property_getter arg1_prop, property_getter arg2_prop)
 {
-	check(obj.v(), v_expected, v_precision, "v",
-			arg1, arg1_val, arg2, arg2_val);
-	check(obj.u(), u_expected, u_precision, "u",
-			arg1, arg1_val, arg2, arg2_val);
-	check(obj.h(), h_expected, h_precision, "h",
-			arg1, arg1_val, arg2, arg2_val);
-	check(obj.s(), s_expected, s_precision, "s",
-			arg1, arg1_val, arg2, arg2_val);
+	check_any(obj, &h2o::H2O::v, v_expected, v_precision,
+			arg1_prop, arg2_prop);
+	check_any(obj, &h2o::H2O::u, u_expected, u_precision,
+			arg1_prop, arg2_prop);
+	check_any(obj, &h2o::H2O::h, h_expected, h_precision,
+			arg1_prop, arg2_prop);
+	check_any(obj, &h2o::H2O::s, s_expected, s_precision,
+			arg1_prop, arg2_prop);
 }
 
 int main(void)
@@ -90,57 +89,57 @@ int main(void)
 			0.112324818E+3, 1E-6,
 			0.115331273E+3, 1E-6,
 			0.392294792E+0, 1E-9,
-			"p", 3., "T", 300);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(80, 300),
 			0.971180894E-3, 1E-12,
 			0.106448356E+3, 1E-6,
 			0.184142828E+3, 1E-6,
 			0.368563852E+0, 1E-9,
-			"p", 80, "T", 300);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(3., 500),
 			0.120241800E-2, 1E-11,
 			0.971934985E+3, 1E-6,
 			0.975542239E+3, 1E-6,
 			0.258041912E+1, 1E-8,
-			"p", 3., "T", 500);
+			&h2o::H2O::p, &h2o::H2O::T);
 
 	check_vuhs(h2o::H2O(35E-4, 300),
 			0.394913866E+2, 1E-7,
 			0.241169160E+4, 1E-5,
 			0.254991145E+4, 1E-5,
 			0.852238967E+1, 1E-8,
-			"p", 35E-4, "T", 300);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(35E-4, 700),
 			0.923015898E+2, 1E-7,
 			0.301262819E+4, 1E-5,
 			0.333568375E+4, 1E-5,
 			0.101749996E+2, 1E-7,
-			"p", 35E-4, "T", 700);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(30E+0, 700),
 			0.542946619E-2, 1E-11,
 			0.246861076E+4, 1E-5,
 			0.263149474E+4, 1E-5,
 			0.517540298E+1, 1E-8,
-			"p", 30E+0, "T", 700);
+			&h2o::H2O::p, &h2o::H2O::T);
 
 	check_vuhs(h2o::H2O(.5, 1500),
 			0.138455090E+1, 1E-8,
 			0.452749310E+4, 1E-5,
 			0.521976855E+4, 1E-5,
 			0.965408875E+1, 1E-8,
-			"p", .5, "T", 1500);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(30, 1500),
 			0.230761299E-1, 1E-10,
 			0.447495124E+4, 1E-5,
 			0.516723514E+4, 1E-5,
 			0.772970133E+1, 1E-8,
-			"p", 30, "T", 1500);
+			&h2o::H2O::p, &h2o::H2O::T);
 	check_vuhs(h2o::H2O(30, 2000),
 			0.311385219E-1, 1E-10,
 			0.563707038E+4, 1E-5,
 			0.657122604E+4, 1E-5,
 			0.853640523E+1, 1E-8,
-			"p", 30, "T", 2000);
+			&h2o::H2O::p, &h2o::H2O::T);
 
 	check_any(h2o::H2O::Tx(300, 1), &h2o::H2O::p, 0.353658941E-2, 1E-11,
 			&h2o::H2O::T, &h2o::H2O::x);
